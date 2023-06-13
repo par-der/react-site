d243fc8f267102ccac3f5eca29c2b474c4cd11f1
Kishou Arima <egorterentev58@gmail.com>
2023-06-13 09:47:14 +0400

    add scroll bottom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, Im <span className="text-[#915eff]">Adrian</span>
+            Hi, Im <span className="text-[#915eff]">Egor</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />{" "}
@@ -24,6 +24,26 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      {/* кнопка передвежения снизу сайта */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0,24,0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+             />
+          </div>
+        </a>
+      </div>
+
     </section>
   );
 };
